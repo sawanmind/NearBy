8ff88223322f524e741331fd01ecd9cde4aed29d
Sawan Kumar <sawanmind@gmail.com>
2024-06-01 10:13:28 +0530

    Initial Commit

---
+++ NearBy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NearBy
+//
+//  Created by bAI on 01/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NearBy/NearByApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearByApp.swift
+//  NearBy
+//
+//  Created by bAI on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NearByApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
