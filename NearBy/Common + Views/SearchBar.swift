c233ac25e5d1bb0ffe464994b975fbfe28d6f6b0
Sawan Kumar <sawanmind@gmail.com>
2024-06-01 10:21:02 +0530

    SearchBar UI component create

---
+++ NearBy/Common + Views/SearchBar.swift
@@ -0,0 +1,60 @@
+//
+//  SearchBar.swift
+//  NearBy
+//
+//  Created by Sawan Kumar on 01/06/24.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @Binding var text: String
+    @Binding var isEditing: Bool
+    
+    var body: some View {
+        HStack {
+            TextField("Search ...", text: $text)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+                        
+                        if isEditing {
+                            Button(action: {
+                                self.text = ""
+                            }) {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                )
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+            
+            if isEditing {
+                Button(action: {
+                    self.isEditing = false
+                    self.text = ""
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }) {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                .animation(.default)
+            }
+        }
+    }
+}
+
+
